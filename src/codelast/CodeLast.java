583f0a6017bb08bc99c4f90d6a7ce25997f8e6c5
343084331 <343084331@C-FQ6ZY72.tdsb.on.ca>
2026-06-03 09:29:31 -0400

    Added code for loading scenes

--- src/codelast/CodeLast.java
+++ src/codelast/CodeLast.java
@@ -15,6 +15,7 @@ public class CodeLast {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Sketch.main("CodeLast.Sketch");
     }
     
 }
---
+++ src/codelast/Sketch.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codelast;
+
+/**
+ *
+ * @author 343084331
+ */
+
+// Importing
+import processing.core.PApplet;
+
+
+public class Sketch extends PApplet {
+    // Object declaration
+    private Human bamboo;
+    private Human father;
+    int stage = 0;
+    
+    // Setting screen
+    public void settings() {
+        size(800, 800);
+    }
+    
+    // Setting up objects and text
+    public void setup () {
+        background(255); // BG colour set to white
+        textSize(20);
+        // Instantiating object
+        bamboo = new Human (this, 400, 400, 2, "Bamboo", "images/human.png");
+    }
+    
+    // Drawing stages
+    public void draw() {
+        background(255); // BG colour set to white
+        if (stage == 0) {
+            fill(0);
+            text("My Cultural Story", width/2, 50);
+            text("Press enter to begin", width/2, 100);
+        } else if (stage == 1) {
+            bamboo.draw(); // Drawing Bamboo character
+        }
+    } // end draw method
+    
+    // Method for different results when using keyboard
+    public void keyPressed () {
+        // Starting screen
+        if (stage == 0) {
+            if (keyCode == ENTER) { // User hits enter
+                stage = 1; // Changing to stage 1
+            }
+        } // End of stage 0 if-statement
+    } // End of key pressed
+} // End of class
