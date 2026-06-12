30cd10b6b50df2dee163cc6ec8d8aa0f2e294b4b
343084331 <343084331@C-FQ6ZY72.tdsb.on.ca>
2026-06-12 10:49:20 -0400

    added comments to sketch

--- src/codelast/Sketch.java
+++ src/codelast/Sketch.java
@@ -9,9 +9,10 @@ package codelast;
  * @author 343084331
  */
 
-// Importing
+// Importing for visuals
 import processing.core.PApplet;
 import processing.core.PImage;
+// Importing to use files
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
@@ -20,7 +21,7 @@ import java.util.Scanner;
 
 public class Sketch extends PApplet {
     // Object declaration
-    private Human bamboo;
+    private Human bamboo; // main character
     private Human father;
     private Animal turtle;
     private Animal dragon;
@@ -28,15 +29,15 @@ public class Sketch extends PApplet {
     // Objects set as character but will use the HeldItem constructor version
     private Character door; // door isn't held but it is used for item checks
     private Character itemKey; // key
-    
+    // PImage is for objects that won't interact with characters
     private PImage bg; // background
     private PImage dialog; // dialogue
-    // Variable to determine which stage to show
-    int stage = 0;
+    // Variable for game function
+    int stage = 0; // tracks current stage
     static int heldStage = 0; // Variable used to keep track of previous stage
-    boolean pickup = false;
+    boolean pickup = false; // determines if player is holding a key
     
-    // Setting screen
+    // Setting screen dimensions
     public void settings() {
         size(400, 400);
     }
@@ -44,8 +45,8 @@ public class Sketch extends PApplet {
     // Setting up objects and text
     public void setup () {
         bg = loadImage ("images/noon.png"); // Setting up BG image
-        textSize(20);
-        // Instantiating object
+        textSize(20); // sets size of text when displayed
+        // Instantiating object using normal constructor
         bamboo = new Human (this, width/2, height/2, 2, "Bamboo", "images/human.png");
         father = new Human (this, 100, 100, 1, "Father", "images/father.png");
         turtle = new Animal (this, 300, 100, 4, "Turtle", "images/turtle.png");
@@ -66,8 +67,9 @@ public class Sketch extends PApplet {
             fill(0);
             text("My Cultural Story", 130, 50);
             text("Press enter to begin", 120, 100);
+            // 1st game scene
         } if (stage == 1) {
-            // Draw objects
+            // Drawing objects
             door.draw();
             itemKey.draw();
             father.draw();
@@ -79,6 +81,7 @@ public class Sketch extends PApplet {
             bg = loadImage ("images/field.png"); 
             // Save progress
             progressSave();
+            // 2nd game scene
         } if (stage == 2) {
             background(50);
             bamboo.draw();
@@ -95,7 +98,8 @@ public class Sketch extends PApplet {
            text("Game is currently paused", 20, 50);
            text("Press Z to unpause", 20, 100);
         }
-        // Character movement
+        
+        // Actions for keyboard input
         if (keyPressed) {
             // Stop player from moving during start and pause
             if (stage == 0 || stage == 10000) {
@@ -103,7 +107,7 @@ public class Sketch extends PApplet {
             } else { // Set speed to normal when not paused
                 bamboo.speed = 2;
             }
-            
+            // When picking up a key, it moves with the player
             if (pickup == true) {
                 itemKey.x = bamboo.x;
                 itemKey.y = bamboo.y;
@@ -114,6 +118,7 @@ public class Sketch extends PApplet {
                 turtle.x = bamboo.x - 20; // Set new position of turtle
                 turtle.y = bamboo.y; // Turtle moves with bamboo while true
             }
+            // Character movement
             // WASD was used for movement since most games use WASD
             if (key == 'w') {
                 bamboo.move(0,-1);
@@ -128,12 +133,13 @@ public class Sketch extends PApplet {
                 bamboo.move(1,0);
             }    
         }
+        
         // Turtle collision check
         if (bamboo.isCollidingWith(turtle)) {
-                // Setting dialogue img with check to see if character is moutned
+                // Setting dialogue img with check to see if character is mounted
                 if (bamboo.mounted == true) {
                     dialog = loadImage("images/mountText.png");
-                } else {
+                } else { // set to normal dialogue when unmounted
                     dialog = loadImage("images/dialog1.PNG"); 
                 }
                 image(dialog, 40, 300); // Showing dialogue
@@ -142,13 +148,14 @@ public class Sketch extends PApplet {
                 if (keyCode == ENTER) {
                     bamboo.mounted = true;
                 }
-                if (keyCode == BACKSPACE) {
+                if (keyCode == BACKSPACE) { // unmounting
                     bamboo.mounted = false;
                     turtle.x = 300;
                     turtle.y = 100;
                 }
             } // End of turtle's collision code
         
+        // stage 1 collision checks
         if (stage == 1) {
             // Check if main character is colliding
             if (bamboo.isCollidingWith(dragon)) {
@@ -190,22 +197,22 @@ public class Sketch extends PApplet {
         // Starting screen
         if (stage == 0) {
             if (keyCode == ENTER) { // User hits enter
-                progressLoad();
-                stage = heldStage;
+                progressLoad(); // loads the stage stored from file
+                stage = heldStage; // sets player's stage to the stored value
             }
         } // End of stage 0 if-statement
         
         // Code for pause menu
         if (stage != 0) {
             if (keyCode == TAB) { // Starts pause
-                heldStage = stage;
+                heldStage = stage; // store the stage the user was on
                 stage = 10000; // Stage is set high so pause menu won't trigger normally
             }  if (key == 'z') { // Ends pause
-                stage = heldStage;
+                stage = heldStage; // sets the stage to the stored one
             }
         } // end of pause code
         
-        // Check for mount and info
+        // Check for mount and info with turtle
         if (stage != 0 && bamboo.isCollidingWith(turtle)) {
             // mount check
             if (key == 'c') {
@@ -250,21 +257,30 @@ public class Sketch extends PApplet {
        }
     }
     
+    /**
+     * Code to check if a character can be mounted
+     * @param c Character that is checked for in the method
+     */
     public void mountCheck (Character c) {
+        // if c is  the human subclass
         if (c instanceof Human) {
             System.out.println("Cannot mount another person");
-        }
+        } // if c is an animal
         if (c instanceof Animal) {
-            if (c == turtle) {
+            if (c == turtle) { // only turtle can be mounted
                 System.out.println("Animal can be mounted");
             }
-            else {
+            else { // text when checking other animals
                 System.out.println("Animal cannot be mounted");
             }
         }
-    }
+    } // ends mountCheck
     
+    /**
+     * Checks character info
+     * @param c Character used by method to display info
+     */
     public void charInfoCheck (Character c) {
         System.out.println(c); // uses the toString method of character
-    }
+    } // end charInfoCheck
 } // End of class
